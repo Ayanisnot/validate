2c0fda08b1dd6bed61af2d1fb13abd4efe13bf80
Ayanisnot <alayanhussain0@gmail.com>
2025-03-22 23:54:52 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let today = new Date();
-let day = today.getDate();
-let month = today.getMonth() + 1;
-let year = today.getFullYear();
+let numbers = [5, 12, 9, 94];
+function myfunction(num)  {
+    return num * 50;
+}
+   
+var newarray = numbers.map(myfunction);
+document.getElementById("product_value").innerHTML = "The mapped values are:" + newarray;
 
-document.getElementById("date").innerHTML = day + "-" + month + "-" + year
+
+
+function ascending() {
+    newarray.sort(function(a, b){return a - b});
+    document.getElementById("product_value").innerHTML = "The ascending order of mapped values are:" + newarray;
+
+}
+function descending() {
+ newarray.sort(function(a, b){return b - a});
+ document.getElementById("product_value").innerHTML = "The descending order of mapped values are:" + newarray;
+
+}
